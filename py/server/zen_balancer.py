56a2077f6f1d4081bc353ae5eaf9e5b0504d10b5
Alberto Sala <alberto.sala@nttdata.com>
2023-06-12 11:10:12 +0200

    Modified impl of get_nsc_ownerships now resorting to a eth_call towards native smart contract instead of http api

--- py/server/zen_balancer.py
+++ py/server/zen_balancer.py
@@ -1,11 +1,15 @@
 import datetime
+import pprint
 import re
 import string
 import sys
 import threading
+from binascii import unhexlify
 
 import base58
 import requests
+from eth_abi import decode
+from eth_utils import remove_0x_prefix, to_checksum_address, function_signature_to_4byte_selector, encode_hex
 
 from flask import Flask, request, json
 from proposal import VotingProposal
@@ -15,6 +19,7 @@ NETWORK = "test"
 #NETWORK = "main"
 
 NSC_URL = "http://zendao-tn-1.de.horizenlabs.io:8200/"
+#NSC_URL = "http://zendao-test-tn-1.us.horizenlabs.io/dev1/"
 #NSC_URL = "http://localhost:8200/"
 
 
@@ -188,7 +193,7 @@ def api_server():
         if mock_nsc:
             ret = MC_ADDRESS_MAP
         else:
-            sc_address = proposal['scAddress']
+            sc_address = remove_0x_prefix(proposal['scAddress'])
             ret = get_nsc_ownerships(sc_address)
         print_outgoing("BalancerApiServer", "/api/v1/getOwnerships", ret)
 
@@ -333,7 +338,7 @@ def api_server():
 
         return chain_height, bestblockhash
 
-    def get_nsc_ownerships(sc_address=None):
+    def get_nsc_ownerships1(sc_address=None):
         # Call eon endpoint /transaction/getKeysOwnership which call NativeSmartContract interface
         request_body = {"scAddressOpt" : sc_address}
         print_outgoing("NSC", "/transaction/getKeysOwnership", request_body)
@@ -341,6 +346,70 @@ def api_server():
         print_incoming("NSC", "/transaction/getKeysOwnership", response.json())
         return response.json()['result']['keysOwnership']
 
+    def get_nsc_ownerships(sc_address=None):
+        if sc_address is None:
+            method = 'getAllKeyOwnerships()'
+            abi_str = encode_hex(function_signature_to_4byte_selector(method))
+        else:
+            method = 'getKeyOwnerships(address)'
+            abi_method_str = encode_hex(function_signature_to_4byte_selector(method))
+            abi_str = abi_method_str + "000000000000000000000000" + sc_address
+
+        request_body = {
+          "jsonrpc": "2.0",
+          "method": "eth_call",
+          "params": [
+            {
+              "from": "0x00c8f107a09cd4f463afc2f1e6e5bf6022ad4600",
+              "to": "0x0000000000000000000088888888888888888888",
+              "value": "0x00",
+              "gasLimit": "0x21000",
+              "maxPriorityFeePerGas": "0x900000000",
+              "maxFeePerGas": "0x900000000",
+              "data": abi_str
+            }
+          ],
+          "id": 1
+        }
+        print_outgoing("NSC", "/ethv1/eth_call (getKeysOwnership)", request_body)
+        response = requests.post(NSC_URL + "ethv1", json.dumps(request_body), auth=('user', 'Horizen'))
+        abiReturnValue = remove_0x_prefix(response.json()['result'])
+
+        ret = get_key_ownership_from_abi(abiReturnValue)
+        print_incoming("NSC", "/ethv1/eth_call (getKeysOwnership)", response.json())
+        return ret
+
+    def hex_str_to_bytes(hex_str):
+        return unhexlify(hex_str.encode('ascii'))
+
+    def get_key_ownership_from_abi(abiReturnValue):
+        # the location of the data part of the first (the only one in this case) parameter (dynamic type), measured in bytes from the start
+        # of the return data block. In this case 32 (0x20)
+        start_data_offset = decode(['uint32'], hex_str_to_bytes(abiReturnValue[0:64]))[0] * 2
+
+        end_offset = start_data_offset + 64  # read 32 bytes
+        list_size = decode(['uint32'], hex_str_to_bytes(abiReturnValue[start_data_offset:end_offset]))[0]
+
+        sc_associations_dict = {}
+        for i in range(list_size):
+            start_offset = end_offset
+            end_offset = start_offset + 192  # read (32 + 32 + 32) bytes
+            (address_pref, mca3, mca32) = decode(['address', 'bytes3', 'bytes32'],
+                                                 hex_str_to_bytes(abiReturnValue[start_offset:end_offset]))
+            sc_address_checksum_fmt = to_checksum_address(address_pref)
+            print("sc addr=" + sc_address_checksum_fmt)
+            if sc_associations_dict.get(sc_address_checksum_fmt) is not None:
+                mc_addr_list = sc_associations_dict.get(sc_address_checksum_fmt)
+            else:
+                sc_associations_dict[sc_address_checksum_fmt] = []
+                mc_addr_list = []
+            mc_addr = (mca3 + mca32).decode('utf-8')
+            mc_addr_list.append(mc_addr)
+            print("mc addr=" + mc_addr)
+            sc_associations_dict[sc_address_checksum_fmt] = mc_addr_list
+
+        pprint.pprint(sc_associations_dict)
+        return (sc_associations_dict)
 
     app.run(host = "0.0.0.0", port = 5000)
 
