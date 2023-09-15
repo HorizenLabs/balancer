14e51b98a15d9d6d3ac9b7e29fec7cda9615a3e6
Alberto Sala <albertosala@horizenlabs.io>
2023-09-15 17:28:29 +0200

    Client points by default to local http server

--- py/client/zen_snapshot_mock.py
+++ py/client/zen_snapshot_mock.py
@@ -13,8 +13,8 @@ LOCAL_HTTP_SERVER_URL = "http://localhost:5000/"
 #LOCAL_HTTP_SERVER_URL = "https://localhost:5000/"
 REMOTE_HTTP_SERVER_URL = "https://zendao-tn-1.de.horizenlabs.io:5000/"
 
-#HTTP_SERVER_URL = LOCAL_HTTP_SERVER_URL
-HTTP_SERVER_URL = REMOTE_HTTP_SERVER_URL
+HTTP_SERVER_URL = LOCAL_HTTP_SERVER_URL
+#HTTP_SERVER_URL = REMOTE_HTTP_SERVER_URL
 
 # if the server is on https we should use a cert
 # ---
